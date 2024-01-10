32875b9a669de98d8a45121a20b96260b022ad11
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-01-10 08:58:41 +0100

    #2 Diseño Login

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { CgBee } from "react-icons/cg";
 import InputForm from './component/InputForm';
 import Footer from './component/Footer';
 import ButtonForm from './component/ButtonForm';
-import TextForm from './component/TextForm';
+import TextAnimatedForm from './component/TextAnimatedForm';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,11 +12,19 @@ function App() {
   return (
     <div className="App">
       <div className="d-flex flex-column min-vh-100 align-items-center justify-content-center">
-        <CgBee className='app-icon' size={128}/>
-        <TextForm text="Upload ..."></TextForm>
-        <InputForm type="text" placeholder="Usuario"></InputForm>
-        <InputForm type="password" placeholder="Contraseña"></InputForm>
-        <ButtonForm text="Iniciar Sesion"></ButtonForm>
+        <div className='container'>
+          <div className='row mt-5 align-items-center'>
+            <div className='col-8'>
+              <TextAnimatedForm text="Prueba ..."></TextAnimatedForm>
+            </div>
+            <div className='col text-center'>
+              <CgBee className='app-icon' size={128}/>
+              <InputForm type="text" placeholder="Usuario"></InputForm>
+              <InputForm type="password" placeholder="Contraseña"></InputForm>
+              <ButtonForm text="Iniciar Sesion"></ButtonForm>
+            </div>
+          </div>
+        </div>
         <Footer></Footer>
       </div>
     </div>
