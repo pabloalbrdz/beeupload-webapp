6ee3295bb13196468e8279ef6c54c168f8bf9cef
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-01-16 19:25:26 +0100

    #2 Diseño Login

--- src/component/form/BackgroundAnimatedForm.js
+++ src/component/form/BackgroundAnimatedForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { React, useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 import './BackgroundAnimatedForm.css'
---
+++ src/component/form/SocialMediaIcon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SocialMediaIcon.css";
+
+import { FaGoogle } from "react-icons/fa";
+
+function SocialMediaIcon(){
+    return(
+        <button className="social-media-btn">
+            <FaGoogle className="social-media-btn-icon"/>
+        </button>
+    );
+}
+
+export default SocialMediaIcon;
