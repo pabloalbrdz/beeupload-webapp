d66c9164c548df60be788d910feead33e04be6fa
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-02-08 07:35:10 +0100

    #11 Diseño Pagina Principal

---
+++ src/component/main/Sidebar.js
@@ -0,0 +1,114 @@
+import {React, useState } from "react";
+import "./Sidebar.css";
+
+import { CgBee } from "react-icons/cg";
+import { FaFileAlt, FaMusic, FaImage, FaVideo, FaSearch, FaRegArrowAltCircleUp, FaUserCircle } from "react-icons/fa";
+import { IoIosSettings } from "react-icons/io";
+import { RiLogoutCircleRFill } from "react-icons/ri";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+function Sidebar(){
+
+    return(
+        <aside className="sidebar-main-aside d-flex flex-column justify-content-between">
+            <div className="sidebar-main-div-logo">
+                <a>
+                    <CgBee size={46}/>
+                </a>
+            </div>
+            <div className="d-flex">
+                <ul className="sidebar-main-nav-main list-unstyled">
+                    <li className="sidebar-main-nav-item">
+                        <a className="sidebar-main-nav-link" data-bs-toggle="collapse" data-bs-target="#collapseFile" aria-expanded="false" aria-controls="collapseFile">
+                            <FaFileAlt size={34}/>
+                        </a>
+                        <ul id="collapseFile" className="sidebar-main-nav-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaSearch size={21}/>
+                                </a>
+                            </li>
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaRegArrowAltCircleUp size={21}/>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="sidebar-main-nav-item">
+                        <a className="sidebar-main-nav-link" data-bs-toggle="collapse" data-bs-target="#collapseMusic" aria-expanded="false" aria-controls="collapseMusic">
+                            <FaMusic size={34}/>
+                        </a>
+                        <ul id="collapseMusic" className="sidebar-main-nav-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaSearch size={21}/>
+                                </a>
+                            </li>
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaRegArrowAltCircleUp size={21}/>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="sidebar-main-nav-item">
+                        <a className="sidebar-main-nav-link" data-bs-toggle="collapse" data-bs-target="#collapsePhoto" aria-expanded="false" aria-controls="collapsePhoto">
+                            <FaImage size={34}/>
+                        </a>
+                        <ul id="collapsePhoto" className="sidebar-main-nav-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaSearch size={21}/>
+                                </a>
+                            </li>
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaRegArrowAltCircleUp size={21}/>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="sidebar-main-nav-item">
+                        <a className="sidebar-main-nav-link" data-bs-toggle="collapse" data-bs-target="#collapseVideo" aria-expanded="false" aria-controls="collapseVideo">
+                            <FaVideo size={34}/>
+                        </a>
+                        <ul id="collapseVideo" className="sidebar-main-nav-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaSearch size={21}/>
+                                </a>
+                            </li>
+                            <li className="sidebar-main-nav-item">
+                                <a className="sidebar-main-nav-link">
+                                    <FaRegArrowAltCircleUp size={21}/>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div className="sidebar-main-nav-bottom">
+                <a className="sidebar-main-nav-link" data-bs-toggle="collapse" data-bs-target="#collapseProfile" aria-expanded="false" aria-controls="collapseProfile">
+                    <FaUserCircle size={34}/>
+                </a>
+                <ul id="collapseProfile" className="sidebar-main-nav-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li className="sidebar-main-nav-item">
+                        <a className="sidebar-main-nav-link">
+                            <IoIosSettings size={28}/>
+                        </a>
+                    </li>
+                    <li className="sidebar-main-nav-item">
+                        <a className="sidebar-main-nav-link">
+                            <RiLogoutCircleRFill size={28}/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+    );
+}
+
+export default Sidebar;
--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./MainPage.css";
+
+import Sidebar from "../component/main/Sidebar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MainPage(){
     return(
-        <h1>hola</h1>
+        <div className="main-page d-flex">
+            <Sidebar></Sidebar>
+            <div className="main p-3">
+                <div className="text-center">
+                    <h1>Pagina</h1>
+                </div>
+            </div>
+        </div>
     );
 }
 
