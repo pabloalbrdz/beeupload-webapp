f7df08538810a307be582e7cb1ee910b561d226b
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-02-14 23:03:33 +0100

    #15 MVC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { WindowController } from './controller/WindowController';
 
 import FormPage from './page/FormPage';
+import MainPage from './page/MainPage';
 
 function App() {
   const [page, changePage] = useState(
@@ -11,6 +12,16 @@ function App() {
       "view": <FormPage changePage={() => WindowController.changePageView("form", changePage)}></FormPage>
     }
   );
+  useEffect(() => {
+    if (window.sessionStorage.getItem("session") != undefined){
+      changePage(
+        {
+          "page": "main",
+          "view": <MainPage></MainPage>
+        }
+      );
+    }
+  }, []);
   return(
     <div>
       {page.view}
