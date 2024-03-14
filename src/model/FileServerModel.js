b93792ec9aa464053fd168f5fec9e3abc4f5ff68
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-03-14 10:58:38 +0100

    #24 Configuracion de Eliminacion De Cuenta

--- src/model/FileServerModel.js
+++ src/model/FileServerModel.js
@@ -150,4 +150,28 @@ export const FileServerModel = {
         }
     },
 
+    async deleteUserFolder(userId){
+        try{
+            const response = await axios.delete(
+                `${fileserverSettings.USER_FOLDER}/deleteUserFolder?userid=${userId}`,
+                {
+                    userid: `${userId}`
+                },
+                {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin' : '*'
+                    }
+                }
+            );
+            return {"status": response.status, "data": response.data};
+        }catch(error){
+            if (error.response){
+                return {"status": error.response.status, "data": error.response.data};
+            }else{
+                return {"status": 400, "data": error.message};
+            }
+        }
+    },
+
 }
