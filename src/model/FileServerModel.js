5500bce3030b29c2fee23969d083ee57fc5e914f
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-03-14 10:53:21 +0100

    #23 Configuracion de Eliminacion De Ficheros

--- src/model/FileServerModel.js
+++ src/model/FileServerModel.js
@@ -28,5 +28,102 @@ export const FileServerModel = {
         }
     },
 
+    async deleteAllUserDocuments(userId){
+        try{
+            const response = await axios.delete(
+                `${fileserverSettings.USER_DOCUMENT}/deleteAllUserDocuments?userid=${userId}`,
+                {
+                    userid: `${userId}`
+                },
+                {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin' : '*'
+                    }
+                }
+            );
+            return {"status": response.status, "data": response.data};
+        }catch(error){
+            if (error.response){
+                return {"status": error.response.status, "data": error.response.data};
+            }else{
+                return {"status": 400, "data": error.message};
+            }
+        }
+    },
+
+    async deleteAllUserMusic(userId){
+        try{
+            const response = await axios.delete(
+                `${fileserverSettings.USER_MUSIC}/deleteAllUserMusic?userid=${userId}`,
+                {
+                    userid: `${userId}`
+                },
+                {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin' : '*'
+                    }
+                }
+            );
+            return {"status": response.status, "data": response.data};
+        }catch(error){
+            if (error.response){
+                return {"status": error.response.status, "data": error.response.data};
+            }else{
+                return {"status": 400, "data": error.message};
+            }
+        }
+    },
+
+
+    async deleteAllUserImage(userId){
+        try{
+            const response = await axios.delete(
+                `${fileserverSettings.USER_IMAGE}/deleteAllUserImages?userid=${userId}`,
+                {
+                    userid: `${userId}`
+                },
+                {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin' : '*'
+                    }
+                }
+            );
+            return {"status": response.status, "data": response.data};
+        }catch(error){
+            if (error.response){
+                return {"status": error.response.status, "data": error.response.data};
+            }else{
+                return {"status": 400, "data": error.message};
+            }
+        }
+    },
+
+    async deleteAllUserVideo(userId){
+        try{
+            const response = await axios.delete(
+                `${fileserverSettings.USER_VIDEO}/deleteAllUserVideos?userid=${userId}`,
+                {
+                    userid: `${userId}`
+                },
+                {
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin' : '*'
+                    }
+                }
+            );
+            return {"status": response.status, "data": response.data};
+        }catch(error){
+            if (error.response){
+                return {"status": error.response.status, "data": error.response.data};
+            }else{
+                return {"status": 400, "data": error.message};
+            }
+        }
+    },
+
 
 }
