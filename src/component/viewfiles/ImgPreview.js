621d09592417bbc178b02e16183210bc2fa1d7a6
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-03-23 19:53:31 +0100

    #27 Visualizar Imagenes

---
+++ src/component/viewfiles/ImgPreview.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./ImgPreview.css";
+import { Modal } from "react-bootstrap";
+
+function ImgPreview({src}){
+    const [showImageModal, setShowImageModal] = useState(false);
+    return(
+        <>
+            <div className="img-preview-div d-flex flex-column" onClick={() => setShowImageModal(true)}>
+                <img className="img-preview-div-img" src={src}></img>
+            </div>
+            <Modal show={showImageModal} onHide={() => setShowImageModal(false)}>
+                <Modal.Body className="img-preview-div-modal d-flex">
+                    <img src={src}></img>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
+
+export default ImgPreview;
