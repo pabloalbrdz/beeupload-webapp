90dce9bcfdae20ef6e275162ec1c5f35f5684e13
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-04-27 18:49:16 +0200

    #37 Eliminar Documentos y Videos

--- src/component/viewfiles/FilePreview.js
+++ src/component/viewfiles/FilePreview.js
@@ -4,6 +4,7 @@ import { AppContext } from "../../context/AppContext";
 
 import { Modal } from "react-bootstrap";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+import ButtonModelMain from "../../component/main/ButtonModelMain";
 
 import "./FilePreview.css";
 
@@ -13,12 +14,14 @@ function FilePreview({type, id, src, title}){
 
     const [showFileModal, setShowFileModal] = useState(false);
 
+    const [showDeleteFileModel, setShowDeleteFileModel] = useState(false);
+
     function deleteFile(e){
         e.preventDefault();
         if (type == "video"){
-
+            alert("Eliminar video");
         }else if (type == "document"){
-
+            alert("Eliminar documento");
         }
     }
 
@@ -27,12 +30,24 @@ function FilePreview({type, id, src, title}){
             <>
                 <div className="filevideo-preview-div col-12 d-flex">
                     <p onClick={() => setShowFileModal(true)}>{title}</p>
-                    <button><MdOutlineDeleteOutline /></button>
+                    <button onClick={() => setShowDeleteFileModel(true)}><MdOutlineDeleteOutline /></button>
                 </div>
                 <Modal className="filevideo-preview-div-modal" show={showFileModal} onHide={() => setShowFileModal(false)} dialogClassName="filevideo-preview-div-modal-dialog">
                     <Modal.Body className="filevideo-preview-div-modal-body d-flex">
                         <video src={src} controls autoPlay></video>
                     </Modal.Body>
+                </Modal>
+                <Modal className="filevideo-delete-div-modal justify-content-center align-items-center" show={showDeleteFileModel} onHide={() => setShowDeleteFileModel(false)} dialogClassName="filevideo-delete-div-modal-dialog">
+                    <Modal.Header className="d-flex justify-content-center">
+                        <Modal.Title>Eliminar Video</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="d-flex justify-content-center">
+                        <p className="modal-body-p">¿Desea eliminar este video?</p>
+                    </Modal.Body>
+                    <Modal.Footer className="d-flex justify-content-center">
+                        <ButtonModelMain text="Salir" onClick={() => setShowDeleteFileModel(false)}></ButtonModelMain>
+                        <ButtonModelMain text="Aceptar" onClick={deleteFile}></ButtonModelMain>
+                    </Modal.Footer>
                 </Modal>
             </>
         );
@@ -41,12 +56,24 @@ function FilePreview({type, id, src, title}){
             <>
                 <div className="filedocument-preview-div col-12 d-flex">
                     <p onClick={() => setShowFileModal(true)}>{title}</p>
-                    <button><MdOutlineDeleteOutline /></button>
+                    <button onClick={() => setShowDeleteFileModel(true)}><MdOutlineDeleteOutline /></button>
                 </div>
                 <Modal className="filedocument-preview-div-modal justify-content-center align-items-center" show={showFileModal} onHide={() => setShowFileModal(false)} dialogClassName="filedocument-preview-div-modal-dialog">
                     <Modal.Body className="filedocument-preview-div-modal-body d-flex">
                         <embed src={src} type="application/pdf"></embed>
                     </Modal.Body>
+                </Modal>
+                <Modal className="filedocument-delete-div-modal justify-content-center align-items-center" show={showDeleteFileModel} onHide={() => setShowDeleteFileModel(false)} dialogClassName="filedocument-delete-div-modal-dialog">
+                    <Modal.Header className="d-flex justify-content-center">
+                        <Modal.Title>Eliminar Documento</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="d-flex justify-content-center">
+                        <p className="modal-body-p">¿Desea eliminar este documento?</p>
+                    </Modal.Body>
+                    <Modal.Footer className="d-flex justify-content-center">
+                        <ButtonModelMain text="Salir" onClick={() => setShowDeleteFileModel(false)}></ButtonModelMain>
+                        <ButtonModelMain text="Aceptar" onClick={deleteFile}></ButtonModelMain>
+                    </Modal.Footer>
                 </Modal>
             </>
         );
