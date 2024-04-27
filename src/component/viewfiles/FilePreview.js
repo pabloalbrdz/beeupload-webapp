69ca47b096e7fa5740675f0097e144e08d529ae1
pabloalbrdz <pabloalbrdz02@gmail.com>
2024-04-27 18:25:58 +0200

    #37 Eliminar Documentos y Videos

--- src/component/viewfiles/FilePreview.js
+++ src/component/viewfiles/FilePreview.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+
+import { AppContext } from "../../context/AppContext";
 
 import { Modal } from "react-bootstrap";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 import "./FilePreview.css";
 
-function FilePreview({type, src, title}){
+function FilePreview({type, id, src, title}){
+
+    const context = useContext(AppContext);
+
     const [showFileModal, setShowFileModal] = useState(false);
+
+    function deleteFile(e){
+        e.preventDefault();
+        if (type == "video"){
+
+        }else if (type == "document"){
+
+        }
+    }
+
     if (type == "video"){
         return(
             <>
-                <div className="filevideo-preview-div col-12 d-flex flex-column" onClick={() => setShowFileModal(true)}>
-                    <p>{title}</p>
+                <div className="filevideo-preview-div col-12 d-flex">
+                    <p onClick={() => setShowFileModal(true)}>{title}</p>
+                    <button><MdOutlineDeleteOutline /></button>
                 </div>
                 <Modal className="filevideo-preview-div-modal" show={showFileModal} onHide={() => setShowFileModal(false)} dialogClassName="filevideo-preview-div-modal-dialog">
                     <Modal.Body className="filevideo-preview-div-modal-body d-flex">
@@ -22,8 +39,9 @@ function FilePreview({type, src, title}){
     }else if (type == "document"){
         return(
             <>
-                <div className="filedocument-preview-div col-12 d-flex flex-column" onClick={() => setShowFileModal(true)}>
-                    <p>{title}</p>
+                <div className="filedocument-preview-div col-12 d-flex">
+                    <p onClick={() => setShowFileModal(true)}>{title}</p>
+                    <button><MdOutlineDeleteOutline /></button>
                 </div>
                 <Modal className="filedocument-preview-div-modal justify-content-center align-items-center" show={showFileModal} onHide={() => setShowFileModal(false)} dialogClassName="filedocument-preview-div-modal-dialog">
                     <Modal.Body className="filedocument-preview-div-modal-body d-flex">
@@ -33,6 +51,7 @@ function FilePreview({type, src, title}){
             </>
         );
     }
+
 }
 
 export default FilePreview;
